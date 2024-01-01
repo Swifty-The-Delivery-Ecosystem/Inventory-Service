ba36c5fb8b2849534651cea8abbd8b4e486a8c51
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-01-01 12:28:26 +0530

    updated vercel.json

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,17 +1,54 @@
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
 const MenuItem = require("../models/menuItemModel");
+const Restaurant = require("../models/restaurantModel");
 
 //@desc additems
 //@route POST /api/additem
 //@access private
 
-
-const addItem = asyncHandler( async (req,res)=>{
+const addItem = asyncHandler(async (req, res) => {
   console.log(req.body);
-  const {name , restaurant_id , type , price , quantity , image_url , description , rating} = req.body;
-  if(!restaurant_id || !name || !type || !price || !quantity || !image_url){res.status(400);
-  throw new Error('All fields are required')}
-  const newItem = await MenuItem.create({name , restaurant_id , type , price , quantity , image_url , description , rating});
+  const {
+    name,
+    restaurant_id,
+    type,
+    price,
+    quantity,
+    image_url,
+    description,
+    rating,
+  } = req.body;
+  //   if (!restaurant_id || !name || !type || !price || !quantity || !image_url) {
+  //     res.status(400);
+  //     throw new Error("All fields are required");
+  //   }
+
+  // Find the restaurant by its ID
+  const restaurant = await Restaurant.findById(restaurant_id);
+
+  if (!restaurant) {
+    res.status(404);
+    throw new Error("Restaurant not found");
+  }
+
+  // Create a new menu item with _id automatically generated
+  const newItem = await MenuItem.create({
+    name,
+    restaurant_id,
+    type,
+    price,
+    quantity,
+    image_url,
+    description,
+    rating,
+  });
+
+  // Add the new item to the restaurant's items array
+  restaurant.items.push(newItem);
+
+  // Save the updated restaurant
+  await restaurant.save();
+
   res.status(201).json(newItem);
 });
 
@@ -38,9 +75,9 @@ const updateItem = asyncHandler(async (req, res) => {
 //@route DELETE /api/contacts/:id
 //@access private
 
-const deleteItem =asyncHandler( async (req,res)=>{
+const deleteItem = asyncHandler(async (req, res) => {
   const item = await MenuItem.findById(req.params.id);
-  if(!item){
+  if (!item) {
     res.status(404);
     throw new Error("Item not found");
   }
@@ -63,10 +100,9 @@ const updateQuantity = asyncHandler(async (req, res) => {
   res.status(200).json(updatedItem);
 });
 
-
 module.exports = {
   addItem,
   updateItem,
   deleteItem,
-  updateQuantity
+  updateQuantity,
 };
