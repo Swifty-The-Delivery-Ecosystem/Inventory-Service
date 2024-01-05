a7de6adb211dcc4408ea0ce939e8542ffee5e691
Aditya062003 <adityavinay@iitbhilai.ac.in>
2024-01-05 13:03:10 +0530

    removed cors error

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const connectDb = require('./config/dbConnection');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
 
 connectDb();
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port,()=>{
-  console.log('listening on port '+process.env.PORT);
+app.listen(port, () => {
+  console.log("listening on port " + process.env.PORT);
+});
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
 app.use(express.json());
 
-
-app.use("/api/customer",require('./routes/customerRoutes'));
-app.use("/api/vendor",require('./routes/vendorRoutes'));
+app.use("/api/customer", require("./routes/customerRoutes"));
+app.use("/api/vendor", require("./routes/vendorRoutes"));
