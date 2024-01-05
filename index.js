78e1008a14df42edd9306d0368101c0c45deff8d
tusharbansal22 <tusharbansal@iitbhilai.ac.in>
2024-01-05 15:18:11 +0530

    feat: cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const connectDb = require('./config/dbConnection');
 
 connectDb();
@@ -10,6 +11,7 @@ app.listen(port,()=>{
   console.log('listening on port '+process.env.PORT);
 });
 
+app.use(cors);
 app.use(express.json());
 
 
