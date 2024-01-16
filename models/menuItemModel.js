0cb8dcc4cfeb41bd8f178b837027c8a9e9819b68
tusharbansal22 <tusharbansal@iitbhilai.ac.in>
2024-01-16 09:45:28 +0530

    fix: models: menuItemModel

--- models/menuItemModel.js
+++ models/menuItemModel.js
@@ -1,41 +1,67 @@
+const { json } = require('express');
 const mongoose = require('mongoose');
 
 
 // TODO : Apply check on rating cant be changed by restaurant
 
 const MenuItemSchema = mongoose.Schema({
-  _id : mongoose.Schema.Types.ObjectId,
   name : {
     type: String,
-    required : [true, "Please enter an MenuItem name"]
+    required: [true, "Please enter a valid name"]
   },
-  restaurant_id:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true,
-    ref: "Restaurant"
-  },
-  type : {
-    type: Number,
-    required : [true, "Please enter 0 for veg 1 for egg 2 for nonveg"],
-  },
-  price : {
-    type: Number,
-    required : [true, "Please enter a price"],
-  },
-  quantity : {
-    type: Number,
-    required : [true, "Please enter a  quantity"],
+  is_veg:{
+    type: Boolean,
   },
   image_url : {
     type: String,
     required : [true, "Provide url for menu item image"]
   },
+  price : {
+    type: Number,
+    required : [true, "Please enter a price"],
+  },
   description : {
     type: String,
+  },
+  quantity : {
+    type: Number,
+    required : [true, "Please enter a  quantity"],
+  },
+  vendor_id:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref: "Restaurant"
   },
   rating : {
     type: Number,
   },
+  number_of_ratings : {
+    type: Number,
+  },
+  tags : {
+    type: [String],
+  },
+  category : {
+    type: String,
+  },
+  is_available : {
+    type: Boolean,
+    default: true
+  },
+  nutritional_values:{
+    type: mongoose.Mongoose.Schema.Types.json
+  },
+  is_healthy: {
+    type: Boolean
+  },
+  on_offer:{
+    type: Boolean
+  },
+  offer_price:{
+    type:Number
+  }
+  
+  
 });
 
 module.exports = mongoose.model('MenuItem',MenuItemSchema);
