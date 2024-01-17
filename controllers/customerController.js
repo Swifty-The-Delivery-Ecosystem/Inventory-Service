f496f54ef4035b61b1d34b55c963a09509847f66
tusharbansal22 <tusharbansal@iitbhilai.ac.in>
2024-01-17 15:43:20 +0530

    fix: minor

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const MenuItem = require("../models/menuItem.model");
-const Vendor = require("../models/vendor.model");
+const Menu = require("../models/vendor.model");
+const Vendor = require("../models/menu.model");
 
 //@desc Get All Vendors
 //@route GET /api/customer/Vendors
@@ -8,6 +9,17 @@ const Vendor = require("../models/vendor.model");
 
 const getVendors = asyncHandler(async (req, res) => {
   try{
+  const id = req.params.id;
+
+  if(id){
+    try{
+    const menu = Menu.find({vendor_id:id});
+    return res.status(200).json(menu);
+    }
+    catch(err){
+      return res.status(500).json({error: "Vendor doesnt exist"});
+    }
+  }
   const {primary_location, tag, is_veg, sort, page=1,pageSize = 10} = req.query;
   const filters = {};
   if(primary_location) {
@@ -31,12 +43,12 @@ const getVendors = asyncHandler(async (req, res) => {
     }
 
   
-  const Vendors = await Vendor
+  const vendors = await Vendor
   .find(filters)
   .sort(sortOptions)
   .skip( (page-1)*pageSize)
   .limit(pageSize);
-  return res.status(200).json(Vendors);
+  return res.status(200).json(vendors);
 }
 catch(err){
   return res.status(500).json({ error: 'Internal Server Error' });
@@ -47,42 +59,24 @@ catch(err){
 //@route GET /api/customer/menuitems
 //@access public
 
-const getMenuItems = asyncHandler(async (req, res) => {
-  const Vendor_id = req.query.Vendor_id;
-  const Vendor = await Vendor.findOne({ _id: Vendor_id });
+// const getMenu = asyncHandler( async (req,res) =>{
+//   const vendor_id = req.id
+// })
 
-  if (!Vendor) {
-    return res.status(404).json({ message: "Vendor not found" });
-  }
-  res.status(200).json(Vendor.items);
-});
-
-//@desc Get Cart Value
-//@route GET /api/customer/cartprice
+//@desc Get All Menu Items of a Vendor
+//@route GET /api/customer/menuitems
 //@access public
 
-const getCartPrice = asyncHandler(async (req, res) => {
-  const Vendor_id = req.query.VendorID;
-  const cartItems = req.query.cartItems;
+// const getMenuItems = asyncHandler(async (req, res) => {
+//   const Vendor_id = req.query.Vendor_id;
+//   const Vendor = await Vendor.findOne({ _id: Vendor_id });
 
-  const Vendor = await Vendor.findOne({ _id: Vendor_id });
+//   if (!Vendor) {
+//     return res.status(404).json({ message: "Vendor not found" });
+//   }
+//   res.status(200).json(Vendor.items);
+// });
 
-  if (!Vendor) {
-    return res.status(404).json({ message: "Vendor not found" });
-  }
 
-  const menuItems = Vendor.items;
-  let totalPrice = 0;
 
-  cartItems.forEach((cartItem) => {
-    const menuItem = menuItems.find((item) => item._id.equals(cartItem));
-
-    if (menuItem) {
-      totalPrice += menuItem.price;
-    }
-  });
-
-  res.status(200).json({ totalPrice });
-});
-
-module.exports = { getVendors, getCartPrice, getMenuItems };
+module.exports = { getVendors };
